2493c47906425942106460ae79c3ce6f76946d71
helena224 <viherhelena@gmail.com>
2021-03-21 13:40:01 +0100

    add CountButton and PointsToWin components to app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './App.css';
 import CountButton from "./components/CountButton";
+import PointsToWin from "./components/PointsToWin";
 
 function App() {
   return (
     <div className="App">
+      <PointsToWin />
+      <CountButton />
       <CountButton />
     </div>
   );
