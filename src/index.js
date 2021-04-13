e8196d33658a02106bddbd208a967387a9399e0b
helena224 <viherhelena@gmail.com>
2021-04-13 16:14:47 +0200

    add style to center app with flexbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/App.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="index">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
